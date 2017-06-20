159cb2f86d1baa34376ccbf1172d6e4a95fc4b15
Maxwell Carlson <bart4282819@gmail.com>
2017-06-20 02:08:39 -0700

    move saftey testing for ai

--- movegeneration.h
+++ movegeneration.h
@@ -45,6 +45,31 @@ private:
 
 //white or black opponent check
     bool whiteOrBlack(std::string tmpPiece, int x, int y);
+
+
+//function that puts all the needed functions below in the right order to check if a move is safe
+    bool safetyCheck(int x, int y, int x2, int y2);
+
+//create bool board to see if move makes king unsafe or not
+    void createKingSafteyBoard();
+
+//reset bool boards so another piece move can be calced
+    bool resetSafetyBoards();
+
+//check if king is safe against bool boards
+    bool isKingSafe();
+
+//mark where is unsafe and figure out who
+    void whiteOrBlack(int x, int y, char wb);
+
+//make sure move is sane before trying to put on bool board
+    bool sanityCheck(int x, int y);
+
+//move sets for calcing safety boards
+    void upDownLeftRightM(int x, int y, char wb);
+    void diagonalM(int x, int y, char wb);
+    void knightM(int x, int y, char wb);
+    void kingM(int x, int y, char wb);
 };
 
 #endif // MOVEGENERATION_H
